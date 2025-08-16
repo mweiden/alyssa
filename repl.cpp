99df2e9bf8b28f6cd3a769dca21a59cf7137b8ff
Matt Weiden <538456+mweiden@users.noreply.github.com>
2025-08-15 22:55:05 -0700

    Fallback to std::getline when Readline is missing

--- repl.cpp
+++ repl.cpp
@@ -1,10 +1,14 @@
+// REPL implementation optionally using GNU Readline
+
 #include <iostream>
 #include <stdexcept>
+#ifdef HAVE_READLINE
 #include <readline/readline.h>
 #if __has_include(<readline/history.h>)
 #include <readline/history.h>
 #else
 extern "C" void add_history(const char*);
+#endif
 #endif
 #include "interpreter.h"
 #include "environment.h"
@@ -18,12 +22,20 @@ int main() {
 
     cout << "Alyssa P. Hacker's LISP REPL" << endl;
 
+#ifdef HAVE_READLINE
     char *raw_line;
     while ((raw_line = readline(">> ")) != nullptr) {
         std::string line(raw_line);
         free(raw_line);
         if (line.empty()) { continue; }
         add_history(line.c_str());
+#else
+    std::string line;
+    while (true) {
+        cout << ">> ";
+        if (!getline(cin, line)) { break; }
+        if (line.empty()) { continue; }
+#endif
         try {
             SExpr result = intr.eval(line);
             cout << "\033[0;32m" << toString(result) << "\033[0m" << endl;
